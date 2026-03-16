7e2d9001e213acce619c41a12c5f0a1008473e54
manfredmoller <manfredsilicon@icloud.com>
2026-03-16 20:45:04 +0000

    fix: bake git config as admin user, mount SSH key for GitHub access
    - git config now set after USER admin in Dockerfile so it writes to /home/admin/.gitconfig
    - SSH key mounted from host /home/manfred/.ssh into /home/admin/.ssh
    - SSH config on host routes github.com to github_aosis key
    - admin user can now push to GitHub from inside container

--- scripts/aosis_start.sh
+++ scripts/aosis_start.sh
@@ -7,6 +7,7 @@ docker run -it --rm \
   --privileged \
   --user admin \
   -v /mnt/nova_ssd/workspaces/isaac_ros-dev:/workspaces/isaac_ros-dev \
+  -v /home/manfred/.ssh:/home/admin/.ssh:ro \
   -v /dev:/dev \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e DISPLAY=$DISPLAY \
